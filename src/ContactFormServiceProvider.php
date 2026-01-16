3fe35e8d0c07fa4b758084bcc7bf472a2c47d4c8
MD Al-Amin <alamindev27@gmail.com>
2026-01-16 23:58:35 +0600

    [INIT Composer]: Compoer init & basic setup & maked service provider and it is a fresh contact form package

---
+++ src/ContactFormServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Alamindev27\ContactForm;
+
+use Illuminate\Support\ServiceProvider;
+
+class ContactFormServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
